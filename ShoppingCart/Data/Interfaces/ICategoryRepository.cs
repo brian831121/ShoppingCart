46bc4aa287866533c33410ee713abb13ef1f4e61
BrianYu <brian.yu@4idps.com>
2019-03-14 20:25:20 +0800

    Create ICategoryRepository.cs

---
+++ ShoppingCart/Data/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Data.Interfaces
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+    }
+}
