6ad21f38f2b8fd2be0ca8f996db60649ed0b0a40
BrianYu <brian.yu@4idps.com>
2019-03-15 00:32:08 +0800

    Create CategoryController.cs

---
+++ ShoppingCart/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Data.Interfaces;
+
+namespace ShoppingCart.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_categoryRepository.GetAll());
+        }
+    }
+}
