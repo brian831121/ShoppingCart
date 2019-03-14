d3cc15bae9e230a06960598a215727b57bc9346d
BrianYu <brian.yu@4idps.com>
2019-03-14 20:25:30 +0800

    Create CategoryRepository.cs

---
+++ ShoppingCart/Data/Repository/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using ShoppingCart.Data.Interfaces;
+using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Data.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+    }
+}
