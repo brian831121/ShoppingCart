e4e656dc34a502c81cfaefbba15bd0a5c641302c
BrianYu <brian.yu@4idps.com>
2019-03-14 20:25:28 +0800

    Create Repository.cs

---
+++ ShoppingCart/Data/Repository/Repository.cs
@@ -0,0 +1,56 @@
+﻿using ShoppingCart.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Data.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext _db;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public int Count(Func<T, bool> predicate)
+        {
+            return _db.Set<T>().Where(predicate).Count();
+        }
+
+        public void Create(T entity)
+        {
+            _db.Add(entity);
+            _db.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _db.Remove(entity);
+            _db.SaveChanges();
+        }
+
+        public IEnumerable<T> Find(Func<T, bool> predicate)
+        {
+            return _db.Set<T>().Where(predicate);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _db.Set<T>();
+        }
+
+        public T GetById(int id)
+        {
+            return _db.Set<T>().Find(id);
+        }
+
+        public void Update(T entity)
+        {
+            _db.Entry(entity).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _db.SaveChanges();
+        }
+    }
+}
