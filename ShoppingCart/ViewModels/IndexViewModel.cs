32ba5b4d3e3b9e55456cf597dc12fd090c0528a3
BrianYu <brian.yu@4idps.com>
2019-03-15 20:58:26 +0800

    Create IndexViewModel.cs

---
+++ ShoppingCart/ViewModels/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.ViewModels
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
