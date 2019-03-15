6d2b9b66ceeb8282be8914e09bbc21126b6bd025
BrianYu <brian.yu@4idps.com>
2019-03-15 20:56:54 +0800

    Create Product.cs

---
+++ ShoppingCart/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        [Range(1,int.MaxValue, ErrorMessage = "價格須高於1元")]
+        public int Price { get; set; }
+        public enum Size { Big=0, Small=1 }
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+    }
+}
