a16f96c879324fc0850bdf22c21deb00d4c6083e
BrianYu <brian.yu@4idps.com>
2019-03-15 20:57:04 +0800

    Create IProductRepository.cs

---
+++ ShoppingCart/Data/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Data.Interfaces
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        IEnumerable<Product> GetAllWithCategory();
+        Product GetWithCategoryById(int id);
+        IEnumerable<Product> GetProductByCategoryId(int id);
+    }
+}
