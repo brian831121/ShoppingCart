ff32d1ba2aa2d4dcc7a73a62a964187890cabf12
BrianYu <brian.yu@4idps.com>
2019-03-15 20:57:35 +0800

    Update CategoryController.cs

--- ShoppingCart/Controllers/CategoryController.cs
+++ ShoppingCart/Controllers/CategoryController.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using ShoppingCart.Data.Interfaces;
+using ShoppingCart.Models;
 
 namespace ShoppingCart.Controllers
 {
     public class CategoryController : Controller
     {
         private readonly ICategoryRepository _categoryRepository;
+        private readonly IHostingEnvironment _hostingEnvironment;
 
-        public CategoryController(ICategoryRepository categoryRepository)
+        public CategoryController(ICategoryRepository categoryRepository, IHostingEnvironment hostingEnvironment)
         {
             _categoryRepository = categoryRepository;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         public IActionResult Index()
@@ -21,10 +25,73 @@ namespace ShoppingCart.Controllers
             return View(_categoryRepository.GetAll());
         }
 
+        public IActionResult Details(int id)
+        {
+            var category = _categoryRepository.GetById(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
         public IActionResult Create()
         {
             return View();
-
         }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _categoryRepository.Create(category);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var category = _categoryRepository.GetById(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            _categoryRepository.Update(category);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var category = _categoryRepository.GetById(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var category = _categoryRepository.GetById(id);
+            _categoryRepository.Delete(category);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
