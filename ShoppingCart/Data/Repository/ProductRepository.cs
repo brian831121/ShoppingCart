99ddcd68f199ce982b0c3896e94257b6d883ab9c
BrianYu <brian.yu@4idps.com>
2019-03-15 20:57:07 +0800

    Create ProductRepository.cs

---
+++ ShoppingCart/Data/Repository/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using ShoppingCart.Data.Interfaces;
+using ShoppingCart.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingCart.Data.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+
+        }
+
+        public IEnumerable<Product> GetAllWithCategory()
+        {
+            return _db.Products.Include(p => p.Category);
+        }
+
+        public Product GetWithCategoryById(int id)
+        {
+            return _db.Products.Include(p => p.Category).FirstOrDefault(m => m.Id.Equals(id));
+        }
+
+        public IEnumerable<Product> GetProductByCategoryId(int id)
+        {
+            return _db.Products.Where(p => p.CategoryId.Equals(id)).Include(p => p.Category);
+        }
+    }
+}
