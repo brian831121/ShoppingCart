e55d99879f63731e168a67e6e44cb2c7891143b0
BrianYu <brian.yu@4idps.com>
2019-03-18 15:07:03 +0800

    Create Order.cs

---
+++ ShoppingCart/Models/Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string ApplicationUserId { get; set; }
+
+        [ForeignKey("ApplicationUserId")]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        public double OrderTotalOriginal { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        [Display(Name = "總計")]
+        public double OrderTotal { get; set; }
+
+        public string Status { get; set; }
+        public string PaymentStatus { get; set; }
+
+
+
+    }
+}
