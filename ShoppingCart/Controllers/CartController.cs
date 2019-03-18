16da0e39a96c2cbb3484d97fce864ab7cf35233f
BrianYu <brian.yu@4idps.com>
2019-03-18 15:49:24 +0800

    Create CartController.cs

---
+++ ShoppingCart/Controllers/CartController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Models;
+using ShoppingCart.ViewModels;
+
+namespace ShoppingCart.Controllers
+{
+    public class CartController : Controller
+    {
+        public OrderItemCartViewModel orderItemCartVM { get; set; }
+        public CartController()
+        {
+
+        }
+        public IActionResult Index()
+        {
+            orderItemCartVM = new OrderItemCartViewModel()
+            {
+                Order = new Order()
+            };
+
+            orderItemCartVM.Order.OrderTotal = 0;
+            orderItemCartVM.Order.OrderTotalOriginal = 0;
+        }
+    }
+}
