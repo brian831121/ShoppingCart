951f6de4ad31a368a5510ad57ea50c59e742f234
BrianYu <brian.yu@4idps.com>
2019-03-18 14:31:07 +0800

    Create ApplicationUserRepository.cs

---
+++ ShoppingCart/Data/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using ShoppingCart.Data.Interfaces;
+using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Data.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+
+        public ApplicationUser GetApplicationUserByEmail(string email)
+        {
+            return _db.AppplicationUser.Where(a => a.Email.Equals(email)).FirstOrDefault();
+        }
+    }
+}
