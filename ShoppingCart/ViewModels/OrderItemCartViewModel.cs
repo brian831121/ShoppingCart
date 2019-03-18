5b1ac24a02665953f6182a7fd22ab62c71dc10e5
BrianYu <brian.yu@4idps.com>
2019-03-18 15:48:13 +0800

    Create OrderItemCartViewModel.cs

---
+++ ShoppingCart/ViewModels/OrderItemCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.ViewModels
+{
+    public class OrderItemCartViewModel
+    {
+        public List<Cart> CardList { get; set; }
+        public Order Order { get; set; }
+    }
+}
