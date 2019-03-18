6fd04408c3371f10d47f8ff2b22cd60d8b2a050d
BrianYu <brian.yu@4idps.com>
2019-03-18 15:48:09 +0800

    Add Order and OrderItem Repository

---
+++ ShoppingCart/Data/Repository/OrderItemRepository.cs
@@ -0,0 +1,21 @@
+﻿using ShoppingCart.Data.Interfaces;
+using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Data.Repository
+{
+    public class OrderItemRepository : Repository<OrderItem>, IOrderItemRepository
+    {
+        public OrderItemRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+
+        public IEnumerable<OrderItem> GetOrderItemsByOrderId(int id)
+        {
+            return _db.OrderItem.Where(o => o.OrderId.Equals(id));
+        }
+    }
+}
---
+++ ShoppingCart/Data/Repository/OrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using ShoppingCart.Data.Interfaces;
+using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Data.Repository
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+
+        public IEnumerable<Order> GetOrdersByUserId(string id)
+        {
+            return _db.Order.Where(o => o.ApplicationUserId.Equals(id));
+        }
+    }
+}
