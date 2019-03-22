9a298e560a39a60e53bcd960eb4192298b5de204
BrianYu <brian.yu@4idps.com>
2019-03-22 17:04:45 +0800

    Create OrderItemViewModel.cs

---
+++ ShoppingCart/ViewModels/OrderItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.ViewModels
+{
+    public class OrderItemViewModel
+    {
+        public Order Order { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+    }
+}
